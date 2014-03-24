04c8840922e3d4d4ca52af29635d43c51b536cd2
Robin Boutros <niuage@gmail.com>
2014-03-24 16:28:02 -0400

    Rails 4 syntax for order clause of has_manys

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -4,7 +4,7 @@ class Gear < ActiveRecord::Base
   belongs_to :build
 
   has_many :gear_gems, dependent: :destroy
-  has_many :skill_gems, through: :gear_gems, order: "(support = true) ASC"
+  has_many :skill_gems, -> { order("(support = true) ASC") }, through: :gear_gems
 
   attr_accessible :name, :description, :skill_gem_ids, :main
 
