990f4eaba44c1538d93d9922a171d5d266100d6a
Robin Boutros <niuage@gmail.com>
2014-04-06 01:07:02 -0400

    deleted a bunch of files

--- db/migrate/20131227145220_create_user_players.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserPlayers < ActiveRecord::Migration
-  def change
-    create_table :user_players do |t|
-      t.references :user
-      t.references :player
-      t.timestamps
-    end
-  end
-end
