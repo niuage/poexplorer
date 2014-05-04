e68d46198bcf6613077bec7cc14ca4ccaf62601c
Robin Boutros <niuage@gmail.com>
2014-05-04 17:13:54 -0400

    Added author to exiles

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,5 +5,7 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: "User", foreign_key: :sender_id
 
   validates :body, presence: true
-  validates :contact_info, presence: { message: "Please provide a way for me to answer you (email, reddit name etc)." }
+  validates :contact_info,
+    length: { maximum: 128 },
+    presence: { message: "Please provide a way for me to answer you (email, reddit name etc)." }
 end
