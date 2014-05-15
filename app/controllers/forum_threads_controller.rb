c47057637374dcb008f9e94690ff6027ff571000
Robin Boutros <niuage@gmail.com>
2014-05-15 16:59:43 -0400

    Load the thread with an ajax request

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,5 +1,5 @@
 class ForumThreadsController < ApplicationController
-  layout "blank"
+  layout :layout
 
   before_filter :view_layout
   after_filter :allow_iframe, only: [:show]
@@ -7,28 +7,33 @@ class ForumThreadsController < ApplicationController
   def show
     return if thread_id == 0
 
-    thread = ForumThread.find_by uid: thread_id
-    league_id = thread.league_id if thread
+    if request.xhr?
+      sleep(3)
+      @js = true
 
-    return unless league_id
+      thread = ForumThread.find_by uid: thread_id
+      league_id = thread.league_id if thread
 
-    @search = Search.new(
-      league_id: league_id, thread_id: thread_id
-    )
+      return unless league_id
 
-    @searchable = Elastic::ItemSearch.new(@search, params)
-    @searchable.any_type = true
-    @tire_search = @searchable.tire_search
+      search = Search.new(
+        league_id: league_id, thread_id: thread_id
+      )
 
-    @results = ItemDecorator.decorate_collection(
-      @tire_search.results
-    )
+      searchable = Elastic::ItemSearch.new(search, params)
+      searchable.any_type = true
+      tire_search = searchable.tire_search
+
+      @results = ItemDecorator.decorate_collection(
+        tire_search.results
+      )
+    end
   end
 
   private
 
   def thread_id
-    params[:id].to_i
+    @thread_id ||= params[:id].to_i
   end
 
   def allow_iframe
@@ -37,6 +42,10 @@ class ForumThreadsController < ApplicationController
 
   def view_layout
     @layout = ViewLayout.new("s", "night")
+  end
+
+  def layout
+    request.xhr? ? false : "blank"
   end
 
 end
