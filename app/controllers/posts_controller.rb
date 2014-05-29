bb544e1e28bc31a5d709cb46e093e7e15df9fbd8
Robin Boutros <niuage@gmail.com>
2014-05-29 13:18:17 -0400

    Removed a lot of files associated with posts and cart feature

--- app/controllers/posts_controller.rb
+++
@@ -1,5 +0,0 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.page(params[:page]).per(10)
-  end
-end
--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :title, :body
-
-  belongs_to :user
-end
